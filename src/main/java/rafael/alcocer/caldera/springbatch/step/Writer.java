fa40ac2c9e1bee6816f7a32f9fbfc4cb126a27f8
rafael-alcocer-caldera <racchec@gmail.com>
2023-05-26 16:36:36 -0600

    Changed year of Copyright

--- src/main/java/rafael/alcocer/caldera/springbatch/step/Writer.java
+++ src/main/java/rafael/alcocer/caldera/springbatch/step/Writer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright [2017] [RAFAEL ALCOCER CALDERA]
+ * Copyright [2023] [RAFAEL ALCOCER CALDERA]
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
